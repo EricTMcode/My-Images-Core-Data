68bf49613ee183a45b6717c89aadfd2a2cc7ea48
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-06-09 14:51:33 +0200

    Initial Commit

---
+++ My Images CD/My_Images_CDApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_Images_CDApp.swift
+//  My Images CD
+//
+//  Created by Eric on 09/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct My_Images_CDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
