2436045497f6fe39e712ed27e7642c6d4969ae7a
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-06-10 13:44:57 +0200

    - Create FileManager, with 3 methods to save, delete and retrieve Image in documents directory

---
+++ My Images CD/Form/FormType.swift
@@ -0,0 +1,31 @@
+//
+//  FormType.swift
+//  My Images CD
+//
+//  Created by Eric on 10/06/2023.
+//
+
+import SwiftUI
+
+enum FormType: Identifiable, View {
+    case new(UIImage)
+    case update(MyImage)
+    
+    var id: String {
+        switch self {
+        case .new:
+            return "new"
+        case .update:
+            return "update"
+        }
+    }
+    
+    var body: some View {
+        switch self {
+        case .new(let uiImage):
+            return ImageFormView(viewModel: FormViewModel(uiImage))
+        case .update(let myImage):
+            return ImageFormView(viewModel: FormViewModel(myImage))
+        }
+    }
+}
